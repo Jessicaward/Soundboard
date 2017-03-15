c57aa4c40d52d9821833618bf39b59f749406e11
Matthew Ward <Matt@matthews-mbp.fritz.box>
2017-03-15 00:28:18 +0000

    Created an initial UI Added a test button Added code to be able to play sounds (it works) B)

--- Soundboard/Soundboard/Soundboard/ViewController.swift
+++ Soundboard/Soundboard/Soundboard/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player:AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func triggerSound(_ sender: Any) {
+        print("testSound");
+        
+        guard let sound = NSDataAsset(name: "testSound") else {
+            print("asset not found")
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(data: sound.data, fileTypeHint: AVFileTypeMPEGLayer3)
+            
+            player!.play()
+        } catch let error as NSError {
+            print("error: \(error.localizedDescription)")
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
